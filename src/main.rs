e1758d66cb119216e1e24b554fefcc743e2c014d
teyoku <teyoku.dev@gmail.com>
2026-08-29 01:00:15 +0500

    add main.rs

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+pub mod parser;
+use std::{env, error::Error, fs};
+
+use crate::{
+    assembler::assemble,
+    parser::{parse, parse_u16},
+    vm::VirtualMachine,
+};
+
+pub mod assembler;
+pub mod error;
+pub mod instruction;
+pub mod register;
+pub mod vm;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 {
+        return Err(format!("Usage: {} <memory-size> <input-file.asm>", args[0]).into());
+    }
+
+    let memory_size = parse_u16(&args[1])?;
+    let code = fs::read_to_string(&args[2])?;
+
+    let program = parse(&code)?;
+    let bytecode = assemble(&program);
+
+    let mut vm = VirtualMachine::new(memory_size as usize);
+    vm.run(&bytecode)?;
+    vm.print_state();
+
+    Ok(())
+}
