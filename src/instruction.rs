3fc3c4c3577196a3960cf3c2be3f1a7c76403466
teyoku <teyoku.dev@gmail.com>
2026-08-30 00:12:28 +0500

    refactor: replace hardcoded opcodes with Opcode enum in Instruction encoding

--- src/instruction.rs
+++ src/instruction.rs
@@ -1,4 +1,4 @@
-use crate::register::Register;
+use crate::{error::RuntimeError, register::Register};
 
 #[derive(Debug, Clone, Copy)]
 pub enum Instruction {
@@ -21,54 +21,96 @@ impl Instruction {
     pub fn encode(&self) -> Vec<u16> {
         let mut bytecode = Vec::new();
         match self {
-            Instruction::Halt => bytecode.push(0),
+            Instruction::Halt => bytecode.push(Opcode::Halt as u16),
             Instruction::Set(register, address) => {
-                bytecode.push((1 << 12) | ((register.as_index() as u16) << 10));
+                bytecode.push(((Opcode::Set as u16) << 12) | ((register.as_index() as u16) << 10));
                 bytecode.push(*address);
             }
             Instruction::Load(register, address) => {
-                bytecode.push((2 << 12) | ((register.as_index() as u16) << 10));
+                bytecode.push(((Opcode::Load as u16) << 12) | ((register.as_index() as u16) << 10));
                 bytecode.push(*address);
             }
             Instruction::Store(register, address) => {
-                bytecode.push((3 << 12) | ((register.as_index() as u16) << 10));
+                bytecode
+                    .push(((Opcode::Store as u16) << 12) | ((register.as_index() as u16) << 10));
                 bytecode.push(*address);
             }
             Instruction::Add(register1, register2) => {
                 let reg1 = (register1.as_index() as u16) << 10;
                 let reg2 = (register2.as_index() as u16) << 8;
-                bytecode.push((4 << 12) | reg1 | reg2);
+                bytecode.push(((Opcode::Add as u16) << 12) | reg1 | reg2);
             }
             Instruction::Sub(register1, register2) => {
                 let reg1 = (register1.as_index() as u16) << 10;
                 let reg2 = (register2.as_index() as u16) << 8;
-                bytecode.push((5 << 12) | reg1 | reg2);
+                bytecode.push(((Opcode::Sub as u16) << 12) | reg1 | reg2);
             }
             Instruction::Jmp(address) => {
-                bytecode.push(6 << 12);
+                bytecode.push((Opcode::Jmp as u16) << 12);
                 bytecode.push(*address);
             }
             Instruction::Jeq(address) => {
-                bytecode.push(7 << 12);
+                bytecode.push((Opcode::Jeq as u16) << 12);
                 bytecode.push(*address);
             }
             Instruction::Jne(address) => {
-                bytecode.push(8 << 12);
+                bytecode.push((Opcode::Jne as u16) << 12);
                 bytecode.push(*address);
             }
             Instruction::Push(register) => {
-                bytecode.push(9 << 12 | ((register.as_index() as u16) << 10));
+                bytecode.push((Opcode::Push as u16) << 12 | ((register.as_index() as u16) << 10));
             }
             Instruction::Pop(register) => {
-                bytecode.push(10 << 12 | ((register.as_index() as u16) << 10));
+                bytecode.push((Opcode::Pop as u16) << 12 | ((register.as_index() as u16) << 10));
             }
             Instruction::Call(address) => {
-                bytecode.push(11 << 12);
+                bytecode.push((Opcode::Call as u16) << 12);
                 bytecode.push(*address);
             }
-            Instruction::Ret => bytecode.push(12 << 12),
+            Instruction::Ret => bytecode.push((Opcode::Ret as u16) << 12),
         }
-        
+
         bytecode
+    }
+}
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+#[repr(u16)]
+pub enum Opcode {
+    Halt = 0,
+    Set = 1,
+    Load = 2,
+    Store = 3,
+    Add = 4,
+    Sub = 5,
+    Jmp = 6,
+    Jeq = 7,
+    Jne = 8,
+    Push = 9,
+    Pop = 10,
+    Call = 11,
+    Ret = 12,
+}
+
+impl TryFrom<u16> for Opcode {
+    type Error = RuntimeError;
+
+    fn try_from(value: u16) -> Result<Self, Self::Error> {
+        match value {
+            0 => Ok(Opcode::Halt),
+            1 => Ok(Opcode::Set),
+            2 => Ok(Opcode::Load),
+            3 => Ok(Opcode::Store),
+            4 => Ok(Opcode::Add),
+            5 => Ok(Opcode::Sub),
+            6 => Ok(Opcode::Jmp),
+            7 => Ok(Opcode::Jeq),
+            8 => Ok(Opcode::Jne),
+            9 => Ok(Opcode::Push),
+            10 => Ok(Opcode::Pop),
+            11 => Ok(Opcode::Call),
+            12 => Ok(Opcode::Ret),
+            _ => Err(RuntimeError::InvalidOpcode(value)),
+        }
     }
 }
