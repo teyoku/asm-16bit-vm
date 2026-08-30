eaace784acd2c7652eca459f2cfc81ada837f35b
teyoku <teyoku.dev@gmail.com>
2026-08-30 21:53:14 +0500

    refactor: streamline memory size parsing in main function

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
 use std::{env, error::Error, fs};
 
-use crate::{
-    assembler::assemble,
-    parser::{parse, parse_u16},
-    vm::VirtualMachine,
-};
+use crate::{assembler::assemble, parser::parse, vm::VirtualMachine};
 
 pub mod assembler;
 pub mod error;
@@ -18,13 +14,13 @@ fn main() -> Result<(), Box<dyn Error>> {
         return Err(format!("Usage: {} <memory-size> <input-file.asm>", args[0]).into());
     }
 
-    let memory_size = parse_u16(&args[1])?;
+    let memory_size = &args[1].parse::<u16>()?;
     let code = fs::read_to_string(&args[2])?;
 
     let program = parse(&code)?;
     let bytecode = assemble(&program);
 
-    let mut vm = VirtualMachine::new(memory_size as usize);
+    let mut vm = VirtualMachine::new(*memory_size as usize);
     vm.memory.load_program(&bytecode, 0)?;
     vm.run()?;
     vm.print_state();
