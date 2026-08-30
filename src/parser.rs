f2f1d123dd9ee3f07fa3477185b4fdf3398b15a5
teyoku <teyoku.dev@gmail.com>
2026-08-30 20:01:16 +0500

    add: labels support

--- src/parser.rs
+++ src/parser.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use crate::{error::AssemblerError, instruction::Instruction, register::Register};
 
 pub fn parse_u16(s: &str) -> Result<u16, AssemblerError> {
@@ -9,7 +11,10 @@ pub fn parse_u16(s: &str) -> Result<u16, AssemblerError> {
     }
 }
 
-fn parse_reg_and_u16(data: &[&str]) -> Result<(Register, u16), AssemblerError> {
+fn parse_reg_and_u16(
+    data: &[&str],
+    labels: &HashMap<String, u16>,
+) -> Result<(Register, u16), AssemblerError> {
     let &reg_str = data.get(1).ok_or(AssemblerError::MissingArgument(
         "Expected register".to_string(),
     ))?;
@@ -18,7 +23,7 @@ fn parse_reg_and_u16(data: &[&str]) -> Result<(Register, u16), AssemblerError> {
     let value_str = data.get(2).ok_or(AssemblerError::MissingArgument(
         "Expected value".to_string(),
     ))?;
-    let value = parse_u16(value_str)?;
+    let value = parse_address_or_label(value_str, labels)?;
 
     Ok((reg, value))
 }
@@ -37,11 +42,11 @@ fn parse_two_regs(data: &[&str]) -> Result<(Register, Register), AssemblerError>
     Ok((reg1, reg2))
 }
 
-fn parse_single_u16(data: &[&str]) -> Result<u16, AssemblerError> {
+fn parse_single_u16(data: &[&str], labels: &HashMap<String, u16>) -> Result<u16, AssemblerError> {
     let address = data.get(1).ok_or(AssemblerError::MissingArgument(
         "Expected address".to_string(),
     ))?;
-    parse_u16(address)
+    parse_address_or_label(address, labels)
 }
 
 fn parse_single_reg(data: &[&str]) -> Result<Register, AssemblerError> {
@@ -51,7 +56,44 @@ fn parse_single_reg(data: &[&str]) -> Result<Register, AssemblerError> {
     reg_str.try_into()
 }
 
+fn parse_address_or_label(s: &str, labels: &HashMap<String, u16>) -> Result<u16, AssemblerError> {
+    if let Some(&addr) = labels.get(s) {
+        return Ok(addr);
+    }
+
+    parse_u16(s).map_err(|_| AssemblerError::UnknownLabel(s.to_string()))
+}
+
 pub fn parse(code: &str) -> Result<Vec<Instruction>, AssemblerError> {
+    let mut labels = HashMap::new();
+    let mut current_address = 0u16;
+
+    for line in code.lines() {
+        // Handling comments
+        let fixed_line = if let Some((part, _)) = line.split_once(';') {
+            part
+        } else {
+            line
+        };
+
+        let data: Vec<&str> = fixed_line.split_whitespace().collect();
+        if data.is_empty() {
+            continue;
+        }
+
+        if data[0].ends_with(':') {
+            if let Some(label) = data[0].strip_suffix(':') {
+                labels.insert(label.to_string(), current_address);
+            }
+        } else {
+            match data[0] {
+                "HALT" | "ADD" | "SUB" | "PUSH" | "POP" | "RET" => current_address += 1,
+                "SET" | "LOAD" | "STORE" | "JMP" | "JEQ" | "JNE" | "CALL" => current_address += 2,
+                _ => (),
+            }
+        }
+    }
+
     let mut program = Vec::new();
 
     for line in code.lines() {
@@ -67,18 +109,22 @@ pub fn parse(code: &str) -> Result<Vec<Instruction>, AssemblerError> {
             continue;
         }
 
+        if data[0].ends_with(':') {
+            continue;
+        }
+
         match data[0] {
             "HALT" => program.push(Instruction::Halt),
             "SET" => {
-                let (reg, value) = parse_reg_and_u16(&data)?;
+                let (reg, value) = parse_reg_and_u16(&data, &labels)?;
                 program.push(Instruction::Set(reg, value));
             }
             "LOAD" => {
-                let (reg, address) = parse_reg_and_u16(&data)?;
+                let (reg, address) = parse_reg_and_u16(&data, &labels)?;
                 program.push(Instruction::Load(reg, address));
             }
             "STORE" => {
-                let (reg, address) = parse_reg_and_u16(&data)?;
+                let (reg, address) = parse_reg_and_u16(&data, &labels)?;
                 program.push(Instruction::Store(reg, address));
             }
             "ADD" => {
@@ -90,15 +136,15 @@ pub fn parse(code: &str) -> Result<Vec<Instruction>, AssemblerError> {
                 program.push(Instruction::Sub(reg1, reg2));
             }
             "JMP" => {
-                let address = parse_single_u16(&data)?;
+                let address = parse_single_u16(&data, &labels)?;
                 program.push(Instruction::Jmp(address));
             }
             "JEQ" => {
-                let address = parse_single_u16(&data)?;
+                let address = parse_single_u16(&data, &labels)?;
                 program.push(Instruction::Jeq(address));
             }
             "JNE" => {
-                let address = parse_single_u16(&data)?;
+                let address = parse_single_u16(&data, &labels)?;
                 program.push(Instruction::Jne(address));
             }
             "PUSH" => {
@@ -110,7 +156,7 @@ pub fn parse(code: &str) -> Result<Vec<Instruction>, AssemblerError> {
                 program.push(Instruction::Pop(reg));
             }
             "CALL" => {
-                let address = parse_single_u16(&data)?;
+                let address = parse_single_u16(&data, &labels)?;
                 program.push(Instruction::Call(address));
             }
             "RET" => program.push(Instruction::Ret),
@@ -141,5 +187,18 @@ mod tests {
             parse("ADD R0 R1 ; don't touch this!").unwrap(),
             vec![Instruction::Add(Register::R0, Register::R1)]
         );
+    }
+
+    #[test]
+    fn test_labels() {
+        let code = "
+        SET R0 10
+        loop_start:
+            SUB R0 R1
+            JNE loop_start
+        HALT
+        ";
+        let program = parse(code).unwrap();
+        assert_eq!(program[2], Instruction::Jne(2));
     }
 }
